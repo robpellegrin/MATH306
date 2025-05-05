588bac636fd0a17125ecfbc70e1ad90a3577dc10
Rob Pellegrin <>
2025-05-05 10:22:54 -0400

    Added CLI args for matrix size, timing functionality.

--- Matrix-CPP/main.cpp
+++ Matrix-CPP/main.cpp
@@ -9,6 +9,8 @@
  *
  */
 
+#include <chrono>
+#include <cstdlib>
 #include <iostream>
 
 #include "Matrix.hpp"
@@ -17,28 +19,44 @@ using std::cout;
 using std::endl;
 
 int main(int argc, char *argv[]) {
-  Matrix A(500, 500);
+  // Default matrix size
+  int size = 500;
+
+  // If an argument is provided, parse it as matrix size
+  if (argc > 1) {
+    size = std::atoi(argv[1]);
+    if (size <= 0) {
+      std::cerr << "Invalid matrix size: " << argv[1] << endl;
+      return 1;
+    }
+  }
+
+  Matrix A(size, size);
   cout << "Matrix Created!" << endl;
 
-  cout << "LU Decomp" << endl;
+  // Record start time
+  auto start = std::chrono::high_resolution_clock::now();
+
+  cout << "LU Decomp (" << size << ") --> OMP" << endl;
   auto PLU = A.luDecompose_opm();
   cout << "LU Decomp Finished!\n";
 
-  // Matrix PA = PLU[0] * A;
-  cout << "Performing P*A\n";
-  Matrix PA = PLU[0].mul_omp(A);
+  // Stop timer
+  auto end = std::chrono::high_resolution_clock::now();
 
-  cout << "Performing L*U\n";
-  Matrix LU = PLU[1].mul_omp(PLU[2]);
+  // Total duration
+  auto duration = end - start;
 
-  // cout << "PA\n\n" << PA << endl;
-  // cout << "LU\n\n" << LU << endl;
+  // Convert to minutes (truncates to whole minutes)
+  auto minutes = std::chrono::duration_cast<std::chrono::minutes>(duration);
 
-  cout << "Verifying\n";
-  if (PA == LU)
-    cout << "PA == LU" << endl;
-  else
-    cout << "PA != LU" << endl;
+  // Convert to seconds and subtract to get the remainder
+  auto seconds = std::chrono::duration_cast<std::chrono::seconds>(  //
+      duration - minutes                                            //
+  );
+
+  cout << "Elapsed time: " << minutes.count() << " minutes and "
+       << seconds.count() << " seconds\n";
 
   return 0;
 }
